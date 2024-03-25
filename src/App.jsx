865a0758168191442b65c8d4f4cebf255a264461
Maximiliano Vera <vera.maxi2002@gmail.com>
2024-03-25 15:10:32 -0300

    transition

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <>
-      <header className="bg-[#FFFFF0] absolute w-full dark:bg-slate-900">
+      <header className="ease-out duration-100 bg-[#FFFFF0] absolute w-full dark:bg-slate-900">
         <Title />
         <ChangeTheme />
         <a
@@ -37,28 +37,28 @@ function App() {
         </a>
       </header>
       <main className="m-0 flex flex-col place-items-center justify-center  h-screen">
-        <h4 className="text-xl pb-2 font-bold sm:hidden dark:text-white ">
+        <h4 className="ease-out duration-100 text-xl pb-2 font-bold sm:hidden dark:text-white ">
           UTN FRRO
         </h4>
-        <h4 className="text-lg pb-2 font-bold sm:hidden dark:text-white">
+        <h4 className="ease-out duration-100 text-lg pb-2 font-bold sm:hidden dark:text-white">
           Sistema de autogestion de alumnos
         </h4>
-        <div className="bg-[#FFFFF0] flex flex-col items-center rounded-3xl p-5 gap-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]  dark:bg-slate-900">
+        <div className="ease-out duration-100 bg-[#FFFFF0] flex flex-col items-center rounded-3xl p-5 gap-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]  dark:bg-slate-900">
           <div className="flex gap-3 flex-col px-10">
-            <label className="text-black font-bold dark:text-white">
+            <label className="ease-out duration-100 text-black font-bold dark:text-white">
               Legajo
             </label>
             <input
-              className="text-black p-1 rounded-lg bg-[#FFFFF0] border-2 border-black focus:outline-none focus:border-sky-600 dark:bg-slate-700 dark:border-gray-500 dark:text-white"
+              className="ease-out duration-100 text-black p-1 rounded-lg bg-[#FFFFF0] border-2 border-black focus:outline-none focus:border-sky-600 dark:bg-slate-700 dark:border-gray-500 dark:text-white"
               type="text"
               autoFocus
             />
-            <label className="text-black font-bold dark:text-white">
+            <label className="ease-out duration-100 text-black font-bold dark:text-white">
               Contraseña
             </label>
             <label className="text-black relative items-center">
               <input
-                className="border-2 border-black p-1 pr-7  bg-[#FFFFF0] rounded-lg focus:outline-none focus:border-sky-600 dark:bg-slate-700 dark:border-gray-500 dark:text-white"
+                className="ease-out duration-100 border-2 border-black p-1 pr-7  bg-[#FFFFF0] rounded-lg focus:outline-none focus:border-sky-600 dark:bg-slate-700 dark:border-gray-500 dark:text-white"
                 type={check}
               />
               <input
@@ -71,7 +71,7 @@ function App() {
             </label>
             <button
               onClick={send}
-              className="bg-sky-600 hover:bg-sky-500 text-white rounded-lg p-2 font-medium cursor-pointer text-lg dark:bg-slate-700 dark:hover:bg-slate-500"
+              className="bg-sky-600 ease-out duration-100  hover:bg-sky-500 text-white rounded-lg p-2 font-medium cursor-pointer text-lg dark:bg-slate-700 dark:hover:bg-slate-500"
             >
               Ingresar
             </button>
--- src/components/ChangeTheme.jsx
+++ src/components/ChangeTheme.jsx
@@ -28,7 +28,7 @@ export const ChangeTheme = () => {
     <button
       onClick={handleChangeTheme}
       //Aca realizar posicionamiento del boton
-      className="absolute top-5 right-5 bg-gray-300 hover:bg-gray-400 rounded-full text-white p-3 dark:bg-gray-700 dark:fill-white dark:hover:bg-slate-500"
+      className="ease-out duration-100 absolute top-5 right-5 bg-gray-300 hover:bg-gray-400 rounded-full text-white p-3 dark:bg-gray-700 dark:fill-white dark:hover:bg-slate-500"
     >
       {viewTheme ? <Moon /> : <Sun />}
     </button>
