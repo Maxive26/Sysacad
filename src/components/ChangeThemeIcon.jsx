3796bef37d63f8d0d6c428333624537ee998d721
Maximiliano Vera <98416036+Maxive26@users.noreply.github.com>
2024-03-31 14:59:55 -0300

    Update ChangeThemeIcon.jsx

--- src/components/ChangeThemeIcon.jsx
+++ src/components/ChangeThemeIcon.jsx
@@ -8,7 +8,7 @@ export const ChangeThemeIcon = () => {
   const [viewTheme, setViewTheme] = useState(false);
 
   useEffect(() => {
-    //Validar localStorage no el theme
+
     if (theme === "dark") {
       document.querySelector("html").classList.add("dark");
       setViewTheme(false);
@@ -26,7 +26,7 @@ export const ChangeThemeIcon = () => {
   return (
     <button
       onClick={handleChangeTheme}
-      //Aca realizar posicionamiento del boton
+ 
       className="ease-out duration-100 absolute top-5 right-5 bg-gray-300 hover:bg-gray-400 rounded-full text-white p-3 dark:bg-gray-700 dark:fill-white dark:hover:bg-slate-500"
     >
       {viewTheme ? <Moon /> : <Sun />}
