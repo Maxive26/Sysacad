2459e9c77650b584ec4c64b9d1b0231df49f163f
Maximiliano Vera <vera.maxi2002@gmail.com>
2024-05-31 21:31:51 -0300

    Cambio de icono

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ export const Title = () => {
         Universidad tecnológica Nacional.
       </h1>
       <h2 className="ease-out duration-100 hidden lg:block lg:text-4xl md:block md:text-xl sm:block font-bold dark:text-white">
-        Facultad regional Rosario
+        Facultad regional Rosario.
       </h2>
       <p className="ease-out duration-100  hidden mt-3 lg:block lg:mt-10 sm:block md:block mb-6 lg:text-xl md:text-base dark:text-white">
         Sistema de autogestión de alumnos
